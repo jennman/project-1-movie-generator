a1ae78dc38124eaafa4cf954af391917b4f4a922
Jennifer Man <jenniferman702@gmail.com>
2022-10-24 14:31:58 -0400

    fetch image

--- src/index.js
+++ src/index.js
@@ -2,20 +2,23 @@
 const moviesUrl = 'http://localhost:3000/movies'
 
 function getMovies () {
-fetch(moviesUrl)
-.then( res => res.json())
-.then( moviesArray => {
-    moviesArray.forEach(movieObject => {
-    const imgNav = document.querySelector('#image-list')
-    const imgBar = document.querySelector('#image-bar')
-    imgBar.src = movieObject.image
-    imgNav.append(imgBar)
-    console.log(imgBar)
-
+    fetch(moviesUrl)
+    .then( res => res.json())
+    .then( moviesArray => {
+        moviesArray.forEach(movieObject => grabMovieImage(movieObject))
     })
-})
-
-
 }
 getMovies()
+
+function grabMovieImage (movieObject){
+    const imgDiv = document.querySelector('#movie-images')
+    // const imgBar = document.querySelector('#image-bar')
+    const img = document.createElement('img')
+    img.src = movieObject.image
+    imgDiv.append(img)
+    console.log(movieObject)
+
+}
+
+
 
